c0f4e207e449363834ad6d0611857ac315932060
Greg Privitera <96251182+GPrivitera84@users.noreply.github.com>
2022-04-30 08:17:22 -0500

    Update SimpleTempGetter.py

--- SimpleTempGetter.py
+++ SimpleTempGetter.py
@@ -2,7 +2,7 @@ import requests
 from tkinter import *
 
 Zip = input("Enter your zipcode")
-apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=32536&appid=f4051fd923412d7b460bfbbaebd1143b&units=metric'
+apiURL = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=f4051fd923412d7b460bfbbaebd1143b&units=metric'.format(Zip)
 res = requests.get(apiURL)
 data = res.json()
 tempCel = data['main']['temp']
